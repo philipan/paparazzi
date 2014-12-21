e151de937e8fe600ddc78d9b0dd60fd7ce1847a7
philipan <a.philipp@geo.uni-augsburg.de>
2014-12-21 08:54:37 +0100

    sd-log for sht and ets-airspeed

--- sw/airborne/modules/sensors/airspeed_ets.c
+++ sw/airborne/modules/sensors/airspeed_ets.c
@@ -47,15 +47,10 @@
 #include "subsystems/datalink/downlink.h"
 #include <math.h>
 
-#ifndef USE_AIRSPEED_ETS
-#if USE_AIRSPEED
-#define USE_AIRSPEED_ETS TRUE
-PRINT_CONFIG_MSG("USE_AIRSPEED_ETS automatically set to TRUE")
+#if !USE_AIRSPEED
+#ifndef AIRSPEED_ETS_SYNC_SEND
+#warning either set USE_AIRSPEED or AIRSPEED_ETS_SYNC_SEND to use ets_airspeed
 #endif
-#endif
-
-#if !USE_AIRSPEED_ETS && !AIRSPEED_ETS_SYNC_SEND
-#warning either set USE_AIRSPEED_ETS or AIRSPEED_ETS_SYNC_SEND to use airspeed_ets
 #endif
 
 #define AIRSPEED_ETS_ADDR 0xEA
@@ -82,6 +77,13 @@ PRINT_CONFIG_VAR(AIRSPEED_ETS_I2C_DEV)
 #endif
 PRINT_CONFIG_VAR(AIRSPEED_ETS_START_DELAY)
 
+#if AIRSPEED_ETS_SDLOG
+#include "sdLog.h"
+#include "subsystems/chibios-libopencm3/chibios_sdlog.h"
+#include "subsystems/gps.h"
+bool_t log_airspeed_ets_started;
+#endif
+
 
 // Global variables
 uint16_t airspeed_ets_raw;
@@ -101,8 +103,7 @@ uint16_t airspeed_ets_cnt;
 uint32_t airspeed_ets_delay_time;
 bool_t   airspeed_ets_delay_done;
 
-void airspeed_ets_init(void)
-{
+void airspeed_ets_init( void ) {
   int n;
   airspeed_ets_raw = 0;
   airspeed_ets = 0.0;
@@ -114,45 +115,44 @@ void airspeed_ets_init(void)
   airspeed_ets_cnt = AIRSPEED_ETS_OFFSET_NBSAMPLES_INIT + AIRSPEED_ETS_OFFSET_NBSAMPLES_AVRG;
 
   airspeed_ets_buffer_idx = 0;
-  for (n = 0; n < AIRSPEED_ETS_NBSAMPLES_AVRG; ++n) {
+  for (n=0; n < AIRSPEED_ETS_NBSAMPLES_AVRG; ++n)
     airspeed_ets_buffer[n] = 0.0;
-  }
 
   airspeed_ets_i2c_trans.status = I2CTransDone;
 
   airspeed_ets_delay_done = FALSE;
   SysTimeTimerStart(airspeed_ets_delay_time);
+
+#if AIRSPEED_ETS_SDLOG
+  log_airspeed_ets_started = FALSE;
+#endif
 }
 
-void airspeed_ets_read_periodic(void)
-{
+void airspeed_ets_read_periodic( void ) {
 #ifndef SITL
   if (!airspeed_ets_delay_done) {
-    if (SysTimeTimer(airspeed_ets_delay_time) < USEC_OF_SEC(AIRSPEED_ETS_START_DELAY)) { return; }
-    else { airspeed_ets_delay_done = TRUE; }
+    if (SysTimeTimer(airspeed_ets_delay_time) < USEC_OF_SEC(AIRSPEED_ETS_START_DELAY)) return;
+    else airspeed_ets_delay_done = TRUE;
   }
-  if (airspeed_ets_i2c_trans.status == I2CTransDone) {
+  if (airspeed_ets_i2c_trans.status == I2CTransDone)
     i2c_receive(&AIRSPEED_ETS_I2C_DEV, &airspeed_ets_i2c_trans, AIRSPEED_ETS_ADDR, 2);
-  }
 #elif !defined USE_NPS
   extern float sim_air_speed;
   stateSetAirspeed_f(&sim_air_speed);
 #endif //SITL
 }
 
-void airspeed_ets_read_event(void)
-{
+void airspeed_ets_read_event( void ) {
   int n;
   float airspeed_tmp = 0.0;
 
   // Get raw airspeed from buffer
   airspeed_ets_raw = ((uint16_t)(airspeed_ets_i2c_trans.buf[1]) << 8) | (uint16_t)(airspeed_ets_i2c_trans.buf[0]);
   // Check if this is valid airspeed
-  if (airspeed_ets_raw == 0) {
+  if (airspeed_ets_raw == 0)
     airspeed_ets_valid = FALSE;
-  } else {
+  else
     airspeed_ets_valid = TRUE;
-  }
 
   // Continue only if a new airspeed value was received
   if (airspeed_ets_valid) {
@@ -164,55 +164,65 @@ void airspeed_ets_read_event(void)
         // Calculate average
         airspeed_ets_offset = (uint16_t)(airspeed_ets_offset_tmp / AIRSPEED_ETS_OFFSET_NBSAMPLES_AVRG);
         // Limit offset
-        if (airspeed_ets_offset < AIRSPEED_ETS_OFFSET_MIN) {
+        if (airspeed_ets_offset < AIRSPEED_ETS_OFFSET_MIN)
           airspeed_ets_offset = AIRSPEED_ETS_OFFSET_MIN;
-        }
-        if (airspeed_ets_offset > AIRSPEED_ETS_OFFSET_MAX) {
+        if (airspeed_ets_offset > AIRSPEED_ETS_OFFSET_MAX)
           airspeed_ets_offset = AIRSPEED_ETS_OFFSET_MAX;
-        }
         airspeed_ets_offset_init = TRUE;
       }
       // Check if averaging needs to continue
-      else if (airspeed_ets_cnt <= AIRSPEED_ETS_OFFSET_NBSAMPLES_AVRG) {
+      else if (airspeed_ets_cnt <= AIRSPEED_ETS_OFFSET_NBSAMPLES_AVRG)
         airspeed_ets_offset_tmp += airspeed_ets_raw;
-      }
     }
     // Convert raw to m/s
 #ifdef AIRSPEED_ETS_REVERSE
-    if (airspeed_ets_offset_init && airspeed_ets_raw < airspeed_ets_offset) {
-      airspeed_tmp = AIRSPEED_ETS_SCALE * sqrtf((float)(airspeed_ets_offset - airspeed_ets_raw)) - AIRSPEED_ETS_OFFSET;
-    }
+    if (airspeed_ets_offset_init && airspeed_ets_raw < airspeed_ets_offset)
+      airspeed_tmp = AIRSPEED_ETS_SCALE * sqrtf( (float)(airspeed_ets_offset-airspeed_ets_raw) ) - AIRSPEED_ETS_OFFSET;
 #else
-    if (airspeed_ets_offset_init && airspeed_ets_raw > airspeed_ets_offset) {
-      airspeed_tmp = AIRSPEED_ETS_SCALE * sqrtf((float)(airspeed_ets_raw - airspeed_ets_offset)) - AIRSPEED_ETS_OFFSET;
-    }
+    if (airspeed_ets_offset_init && airspeed_ets_raw > airspeed_ets_offset)
+      airspeed_tmp = AIRSPEED_ETS_SCALE * sqrtf( (float)(airspeed_ets_raw-airspeed_ets_offset) ) - AIRSPEED_ETS_OFFSET;
 #endif
-    else {
+    else
       airspeed_tmp = 0.0;
-    }
     // Airspeed should always be positive
-    if (airspeed_tmp < 0.0) {
+    if (airspeed_tmp < 0.0)
       airspeed_tmp = 0.0;
-    }
     // Moving average
     airspeed_ets_buffer[airspeed_ets_buffer_idx++] = airspeed_tmp;
-    if (airspeed_ets_buffer_idx >= AIRSPEED_ETS_NBSAMPLES_AVRG) {
+    if (airspeed_ets_buffer_idx >= AIRSPEED_ETS_NBSAMPLES_AVRG)
       airspeed_ets_buffer_idx = 0;
-    }
     airspeed_ets = 0.0;
-    for (n = 0; n < AIRSPEED_ETS_NBSAMPLES_AVRG; ++n) {
+    for (n = 0; n < AIRSPEED_ETS_NBSAMPLES_AVRG; ++n)
       airspeed_ets += airspeed_ets_buffer[n];
-    }
     airspeed_ets = airspeed_ets / (float)AIRSPEED_ETS_NBSAMPLES_AVRG;
-#if USE_AIRSPEED_ETS
+#if USE_AIRSPEED
     stateSetAirspeed_f(&airspeed_ets);
 #endif
-#if AIRSPEED_ETS_SYNC_SEND
+#ifdef AIRSPEED_ETS_SYNC_SEND
     DOWNLINK_SEND_AIRSPEED_ETS(DefaultChannel, DefaultDevice, &airspeed_ets_raw, &airspeed_ets_offset, &airspeed_ets);
 #endif
   } else {
     airspeed_ets = 0.0;
   }
+
+
+#if AIRSPEED_ETS_SDLOG
+  if (pprzLogFile.fs != NULL) {
+    if (!log_airspeed_ets_started) {
+      sdLogWriteLog(&pprzLogFile, "Airspeed(m/s) GPS_fix TOW(ms) Week Lat(1e7rad) Lon(1e7rad) HMSL(mm) gpseed(cm/s) course(1e7rad) climb(cm/s)\n");
+      log_airspeed_ets_started = TRUE;
+    }
+    else {
+      sdLogWriteLog(&pprzLogFile, "airspeed: %d %d %8.4f   %d %d %d   %d %d %d   %d %d %d\n",
+          airspeed_ets_raw, airspeed_ets_offset, airspeed_ets,
+          gps.fix, gps.tow, gps.week,
+          gps.lla_pos.lat, gps.lla_pos.lon, gps.hmsl,
+          gps.gspeed, gps.course, -gps.ned_vel.z);
+    }
+  }
+#endif
+
+
 
   // Transaction has been read
   airspeed_ets_i2c_trans.status = I2CTransDone;
